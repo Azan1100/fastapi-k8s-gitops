c9420f3fdcfc8d3668eb28a90a0812e7d99546a2
Azan1100 <waniazan71@gmail.com>
2026-04-01 11:34:08 +0530

    Fix OTEL Exporter

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+
+# OpenTelemetry imports
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
+# 1. Set tracer provider
+trace.set_tracer_provider(TracerProvider())
+
+# 2. Configure exporter (THIS SENDS DATA TO SIGNOZ)
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://signoz-otel-collector.signoz:4317",
+    insecure=True
+)
+
+# 3. Add span processor
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# 4. Instrument FastAPI
 FastAPIInstrumentor.instrument_app(app)
 
 @app.get("/")
 def root():
-    return {"message": "Hello from K8s + GitOps 🚀"}
+    return {"message": "Tracing works 🚀"}
